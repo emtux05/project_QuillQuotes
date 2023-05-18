cc3c7bb4a982afad96afe8844a841ed1b39a77d5
emtux05 <109680021+emtux05@users.noreply.github.com>
2023-05-18 17:28:30 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,31 @@
+// import React from 'react'
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+import '@styles/global.css';
+
+
+export const metadata = {
+    title: 'Promtopia',
+    description: 'Discover and share AI prompt'
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+        <body>
+            <Provider>
+
+            <div className='main'>
+                <div className='gradiant'/>
+            </div>
+            <main className='app'>
+                <Nav />
+                {children}
+            </main>
+            </Provider>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
